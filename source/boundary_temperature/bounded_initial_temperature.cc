6d43f1ce7a0a11e23364dc8595a94d8c712dfc54
Menno Fraters <menno.fraters@tutanota.com>
2022-03-09 14:00:56 -0800

    Add boundary plugins.

---
+++ source/boundary_temperature/bounded_initial_temperature.cc
@@ -0,0 +1,125 @@
+/*
+  Copyright (C) 2011 - 2020 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/boundary_temperature/bounded_initial_temperature.h>
+#include <aspect/initial_temperature/interface.h>
+
+
+namespace aspect
+{
+  namespace BoundaryTemperature
+  {
+// ------------------------------ InitialTemperature -------------------
+
+    template <int dim>
+    double
+    BoundedInitialTemperature<dim>::
+    boundary_temperature (const types::boundary_id,
+                          const Point<dim> &position) const
+    {
+      return std::max(std::min(this->get_initial_temperature_manager().initial_temperature(position),max_temperature),min_temperature);
+    }
+
+
+    template <int dim>
+    double
+    BoundedInitialTemperature<dim>::
+    minimal_temperature (const std::set<types::boundary_id> &) const
+    {
+      return min_temperature;
+    }
+
+
+
+    template <int dim>
+    double
+    BoundedInitialTemperature<dim>::
+    maximal_temperature (const std::set<types::boundary_id> &) const
+    {
+      return max_temperature;
+    }
+
+
+
+    template <int dim>
+    void
+    BoundedInitialTemperature<dim>::declare_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Boundary temperature model");
+      {
+        prm.enter_subsection("Initial temperature");
+        {
+          prm.declare_entry ("Minimal temperature", "0.",
+                             Patterns::Double (),
+                             "Minimal temperature. Units: \\si{\\kelvin}.");
+          prm.declare_entry ("Maximal temperature", "3773.",
+                             Patterns::Double (),
+                             "Maximal temperature. Units: \\si{\\kelvin}.");
+        }
+        prm.leave_subsection ();
+      }
+      prm.leave_subsection ();
+    }
+
+
+    template <int dim>
+    void
+    BoundedInitialTemperature<dim>::parse_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Boundary temperature model");
+      {
+        prm.enter_subsection("Initial temperature");
+        {
+          min_temperature = prm.get_double ("Minimal temperature");
+          max_temperature = prm.get_double ("Maximal temperature");
+        }
+        prm.leave_subsection ();
+      }
+      prm.leave_subsection ();
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace BoundaryTemperature
+  {
+    ASPECT_REGISTER_BOUNDARY_TEMPERATURE_MODEL(BoundedInitialTemperature,
+                                               "bounded initial temperature",
+                                               "A model in which the temperature at the boundary "
+                                               "is chosen to be the same as given in the initial "
+                                               "conditions."
+                                               "\n\n"
+                                               "Because this class simply takes what the initial "
+                                               "temperature had described, this class can not "
+                                               "know certain pieces of information such as the "
+                                               "minimal and maximal temperature on the boundary. "
+                                               "For operations that require this, for example in "
+                                               "post-processing, this boundary temperature model "
+                                               "must therefore be told what the minimal and "
+                                               "maximal values on the boundary are. This is done "
+                                               "using parameters set in section ``Boundary temperature model/Initial temperature''."
+					       "The only difference between this plugin and the initial temperature "
+					       "plugin is that this plugin foces the initial condition "
+					       "to be within the bounds.")
+  }
+}
